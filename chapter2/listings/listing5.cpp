f8a6ea65e5473a2560c4727adec387ee18e3c792
Ahmed Ouardi <159943000+ouahmedev48@users.noreply.github.com>
2025-10-28 18:58:01 +0100

    Listing2.5 LimitsDemo

---
+++ chapter2/listings/listing5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <limits>
+using namespace std;
+
+int main() {
+
+    
+    cout << "INT_MAX is " << __INT_MAX__ << endl;
+    cout << "LONG_MAX is " << __LONG_MAX__ << endl;
+
+    cout << "FLT_MIN is " << __FLT_MIN__ << endl;
+    cout << "FLT_MAX is " << __FLT_MAX__ << endl;
+
+    cout << "DBL_MIN is " << __DBL_MIN__ << endl;
+    cout << "DBL_MAX is " << __DBL_MAX__ << endl;
+    
+    return 0; 
+}
