3a2166eea8cfee6dd69ed7e0b786f7ba1b19b6f5
Ahmed Ouardi <159943000+ouahmedev48@users.noreply.github.com>
2025-11-13 22:45:24 +0100

    I solve the problem numbner 18

---
+++ chapter2/exercises/exercise18.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    cout << "x          " << "y         " << "pow(9x, y)" << endl;
+    cout << "2.5" << "        " << "1.2" << "       " << pow(2.5, 1.2) << endl;
+    cout << "5.0" << "        " << "2.4" << "       " << pow(5.0, 2.4) << endl;
+    cout << "1.2" << "        " << "3.6" << "       " << pow(1.2, 3.6) << endl;
+    cout << "2.4" << "        " << "5.0" << "       " << pow(2.4, 5.0) << endl;
+    cout << "3.6" << "        " << "2.5" << "       " << pow(3.6, 2.5) << endl;
+    
+    return 0;
+}
